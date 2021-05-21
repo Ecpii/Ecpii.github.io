9614841023a71161e9ce1ada190d83d567a1b726
Wayne He <28408948+Ecpii@users.noreply.github.com>
2021-05-21 18:55:22 -0400

    first iteration of home page video playing and caption
    todo:
    find a less jank way to make the video cover the screen
    smooth transitions
    maybe think about making the first text less obtrusive

---
+++ final/src/index.js
@@ -0,0 +1,15 @@
+const video = document.querySelector("video")
+const taglines = [
+    "fast-paced stacker",
+    "competitive puzzle game",
+    "visual effects joyride"
+]
+
+video.addEventListener("pause", handleVideoEnd)
+
+function handleVideoEnd() {
+    const nextVideoNum = (parseInt(this.currentSrc.charAt(this.currentSrc.length - 5)) + 1) % 3
+    this.src = `../public/${nextVideoNum}.mp4`
+    document.querySelector("#tagline").innerHTML = taglines[nextVideoNum]
+    this.load()
+}
